5541cbfd5cac2264b2380ff63182e9d744d19082
Harsh Wardhan <wardhan071@gmail.com>
2024-03-19 09:48:34 +0530

    first commit

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ const express = require('express');
 
 const app = express();
 const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
+
+//routers
+const authRouter = require('./routes/auth');
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
+app.use('/api/v1/auth', authRouter);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 const start = async () => {
   try {
